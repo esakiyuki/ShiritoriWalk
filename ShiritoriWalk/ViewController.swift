55d183a29ae6ef53e9def82df936bb65d325abea
Yuki Esaki <yesaki610@gmail.com>
2020-05-19 21:01:41 +0900

    Initial Commit

---
+++ ShiritoriWalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShiritoriWalk
+//
+//  Created by esaki yuki on 2020/05/19.
+//  Copyright © 2020 esaki yuKki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
