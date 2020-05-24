c2a81243d202acb8db084543836154f23c3c5c09
Yuki Esaki <yesaki610@gmail.com>
2020-05-25 06:54:09 +0900

    ランキング画面の作成、ランダムで文字を表示する機能の追加、TableViewControllerとTableViewCellの追加 もどるボタンが反応しないこととCellを追加しても1つしか表示されないことが問題点

---
+++ ShiritoriWalk/ScoreViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ScoreViewController.swift
+//  ShiritoriWalk
+//
+//  Created by esaki yuki on 2020/05/25.
+//  Copyright © 2020 esaki yuKki. All rights reserved.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//    @IBAction func toTop() {
+//        self.dismiss(animated: true, completion: nil)
+//    }
+//    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
